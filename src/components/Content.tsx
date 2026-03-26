130b0b17cd101906883f56e2452f3f19cc494d5c
juntingwei <107373152+juntingwei@users.noreply.github.com>
2026-03-26 19:24:20 +1300

    Fix mobile header

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,8 +1,8 @@
 function Content() {
     return (
         <section
-            className="flex min-h-dvh items-center justify-center text-center">
-            <h2 className='text-5xl'>Photos and music to follow the conclusion of the wedding</h2>
+            className="flex min-h-dvh items-center justify-center text-center p-4">
+            <h2 className='text-3xl'>Photos and music to follow the conclusion of the wedding</h2>
         </section>
     )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,11 +6,10 @@ function Hero() {
         <Parallax strength={400} className='bg-olive-400 text-white h-dvh flex flex-col justify-center items-center text-center
         '>
             <Background className='h-dvh w-dvw sepia-30 saturate-40 contrast-50 animate-zoom-in'>
-                <img src={backgroundImage} alt='' className='object-cover w-dvw h-dvh pointer-events-none'/>
+                <img src={backgroundImage} alt='' className='object-cover w-vw h-dvh pointer-events-none'/>
             </Background>
             <div className="animate-slide-fade-in">
-                <h1 className='text-9xl uppercase'>Irene <br/><span className='font-cursive lowercase leading-[0.4] block'>and</span> Junting</h1>
-                <p className='uppercase text-3xl'>are getting married!</p>
+                <h1 className='text-7xl font-thin uppercase mb-2'>Irene <br/><span className='font-cursive lowercase leading-[0.4] block'>and</span> Junting</h1>
             </div>
         </Parallax>
     )
